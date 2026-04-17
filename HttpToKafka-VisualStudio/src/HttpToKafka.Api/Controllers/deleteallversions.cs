ff42d1dbfdcf4c7a5fc2468b871ea3ac1ba91a9a
jimiwal <robert.gamibrodzki@gmail.com>
2026-04-17 13:19:54 +0200

    Create deleteallversions.cs

---
+++ HttpToKafka-VisualStudio/src/HttpToKafka.Api/Controllers/deleteallversions.cs
@@ -0,0 +1,94 @@
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+public class EcsVersionCleanup
+{
+    public static IAmazonS3 CreateS3Client()
+    {
+        var serviceUrl = "https://ecs.example.com";
+        var accessKey = "ECS_ACCESS_KEY";
+        var secretKey = "ECS_SECRET_KEY";
+        var region = "us-east-1";
+
+        var credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        var config = new AmazonS3Config
+        {
+            ServiceURL = serviceUrl,
+            ForcePathStyle = true,
+            AuthenticationRegion = region,
+            SignatureVersion = "4"
+        };
+
+        return new AmazonS3Client(credentials, config);
+    }
+
+    public static async Task DeleteAllObjectVersionsAsync(
+        IAmazonS3 s3,
+        string bucket,
+        CancellationToken ct)
+    {
+        string? keyMarker = null;
+        string? versionIdMarker = null;
+
+        do
+        {
+            var listRequest = new ListVersionsRequest
+            {
+                BucketName = bucket,
+                KeyMarker = keyMarker,
+                VersionIdMarker = versionIdMarker
+            };
+
+            var response = await s3.ListVersionsAsync(listRequest, ct);
+
+            var toDelete = new List<KeyVersion>();
+
+            foreach (var v in response.Versions)
+            {
+                toDelete.Add(new KeyVersion
+                {
+                    Key = v.Key,
+                    VersionId = v.VersionId
+                });
+            }
+
+            foreach (var d in response.DeleteMarkers)
+            {
+                toDelete.Add(new KeyVersion
+                {
+                    Key = d.Key,
+                    VersionId = d.VersionId
+                });
+            }
+
+            foreach (var batch in toDelete.Chunk(1000))
+            {
+                var deleteRequest = new DeleteObjectsRequest
+                {
+                    BucketName = bucket,
+                    Objects = batch.ToList()
+                };
+
+                var deleteResponse = await s3.DeleteObjectsAsync(deleteRequest, ct);
+                Console.WriteLine($"Deleted {deleteResponse.DeletedObjects.Count} versions/delete markers");
+            }
+
+            keyMarker = response.IsTruncated ? response.NextKeyMarker : null;
+            versionIdMarker = response.IsTruncated ? response.NextVersionIdMarker : null;
+
+        } while (keyMarker != null || versionIdMarker != null);
+    }
+
+    public static async Task Main()
+    {
+        var bucketName = "my-bucket";
+
+        using var s3 = CreateS3Client();
+
+        await DeleteAllObjectVersionsAsync(s3, bucketName, CancellationToken.None);
+
+        Console.WriteLine("Finished deleting all object versions.");
+    }
+}
